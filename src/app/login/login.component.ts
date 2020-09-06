aea50ac6d3e0317df9a0fefb2c25a1d4cc764f18
karlalan <hkiomdeagmia@hotmail.com>
2020-09-07 00:03:56 +0900

    issue #6

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
 import { AccountService } from '@app/_services_';
 
@@ -24,6 +25,39 @@ export class LoginComponent implements OnInit {
     ) { }
 
     ngOnInit(): void {
+        this.form = this.formBuilder.group({
+            name: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
     }
 
+    // convenience getter for easy access to form fields
+    get f() { return this.form.controls; }
+
+    onSubmit() {
+        this.isSubmitted = true;
+
+        // reset alerts on submit
+        // this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.isLoading = true;
+        this.accountService.login(this.f.name.value, this.f.password.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.router.navigate([this.returnUrl]);
+                },
+                error => {
+                    // this.alertService.error(error);
+                    this.isLoading = false;
+                });
+    }
 }
